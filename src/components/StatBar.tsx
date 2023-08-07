e8fef19e8937fd3264643a31ae4d5a47bd40e0cf
tailselrayo <rodtailselrayo@gmail.com>
2023-08-07 17:34:56 +0200

    feat(app):finalized pokedex

--- src/components/StatBar.tsx
+++ src/components/StatBar.tsx
@@ -1,4 +1,4 @@
-import { Box, HoverCard, Tooltip } from "@mantine/core";
+import {Box, Progress, Text } from "@mantine/core";
 
 interface StatBarProps {
     size?: number;
@@ -10,16 +10,16 @@ export function StatBar(props: StatBarProps) {
     return (
         <Box
             bg="gray.1"
-            h={5}
-            w={props.size ?? 150}
-        >
-            <Tooltip label={`${props.value}`}>
-                <Box
-                    bg={props.color}
-                    h="100%"
-                    w={(props.size ?? 150) * props.value / 150}
-                ></Box>
-            </Tooltip>
+            w={props.size??150}
+            h={20}
+        >  
+            <Box
+                bg={props.color}
+                w={(props.size??150)*props.value/150}
+                h="100%"
+            >
+                <Text p={0} ta="center" fz={15} c="dark.8">{props.value}</Text>
+            </Box>
         </Box>
     )
 }
--- src/utils/getStatLabel.ts
+++ src/utils/getStatLabel.ts
@@ -2,7 +2,7 @@ export function getStatLabel(name: string) {
     if (name==="attack") return "atk"
     else if (name==="defense") return "def"
     else if (name==="special-attack") return "sp.atk"
-    else if (name==="special-defense") return "sp.defense"
+    else if (name==="special-defense") return "sp.def"
     else if (name==="speed") return "spd"
     else return "hp"
 }
